47dfbe5ad5ed5bb0d930246af9f3b816f5bba5a1
droldan <droldan@cells.es>
2017-05-02 13:49:22 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -7,20 +7,18 @@ def main():
 
     def read(fname):
         return open(os.path.join(os.path.dirname(__file__), fname)).read()
-    with open('PyMocoDS/VERSION') as f:
-        version = f.readline().strip()
 
     setup(
         name='MocoDS',
         packages= find_packages(),
-        version = version,    
-        long_description=read('README.md'),
+        version = '1.2',    
+        long_description='long_description test',
         scripts=[],
         data_files=[],
         url='http://www.cells.es',
         author='CTBeamlines',
         author_email='ctbeamlines@cells.es',
-        description='This package contains MoCo DS'
+        description='This package contains MoCo DS',
         platforms = "all",        
         include_package_data = True,
     )
